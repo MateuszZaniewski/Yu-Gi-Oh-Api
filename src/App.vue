153e08f25c892a6bc87b7afb70e9f321752985d2
MateuszZaniewski <mateusz.zaniewski94@gmail.com>
2023-04-16 13:29:13 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+
+import axios from 'axios'
+import {ref} from 'vue'
+let cardsArray = ref([])
+
+axios.get('cards.json')
+  .then(response => {
+    cardsArray.value = response.data.data
+    console.log(cardsArray)
+  })
+  .catch(error => {
+    console.log(error)
+  });
+</script>
+
+<template>
+  <div>
+    <!-- {{cardsArray}} -->
+  </div>
+
+  <div class="singleImage" v-for="card in cardsArray">
+    {{ card.id }}
+    {{ card.name }}
+    {{ card.race }}
+    {{ card.type }}
+  </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue'
+
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <div>
+    <p>xD</p>
+  </div>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
